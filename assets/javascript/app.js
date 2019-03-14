3ae7c0a353ac9bf049ab9ea11bcf9eb245d1d57e
Jaime Zepeda <nnk168@gmail.com>
2019-03-14 14:26:47 -0500

    drop down and button clicks

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,15 @@
 
 
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBjkO2qYjqUWPq8GyF1pfN_tfR55laNTC8",
+    authDomain: "nba-project-one.firebaseapp.com",
+    databaseURL: "https://nba-project-one.firebaseio.com",
+    projectId: "nba-project-one",
+    storageBucket: "nba-project-one.appspot.com",
+    messagingSenderId: "344444587997"
+};
+firebase.initializeApp(config);
 
 
 var params = {
@@ -29,6 +39,9 @@ $.ajax({
         $("#submit").on("click", function (event) {
             event.preventDefault()
             console.log("ive been clicked")
+
+
+
 
 
 
