186c894646458937c5c75194af6cbd164d789a2f
Sarah Tam <sarahtam718@gmail.com>
2019-03-15 10:14:01 -0500

    cleanest copy so far

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,51 +1,3 @@
-<<<<<<< HEAD
-
-
-var params = {
-    // Request parameters
-};
-
-$.ajax({
-    url: "https://api.fantasydata.net/v3/nba/scores/json/TeamSeasonStats/2019" + $.param(params),
-    beforeSend: function (xhrObj) {
-        // Request headers
-        xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "ea390d2b909e42919428428303b716b8");
-    },
-    type: "GET",
-    // Request body
-    data: "{body}",
-})
-    .done(function (data) {
-
-
-        for (var i = 0; i < data.length; i++)
-            $('<option/>').val(data[i].Name).html(data[i].Name).appendTo('#TeamControlSelect');
-
-        console.log(data[0].Name);
-        console.log(data[0].Wins);
-
-
-        $("#submit").on("click", function (event) {
-            event.preventDefault()
-            console.log("ive been clicked")
-
-
-
-
-
-
-        })
-
-
-
-
-
-
-    })
-    .fail(function () {
-        alert("error");
-    });
-=======
 // Initialize Firebase
 var config = {
   apiKey: "AIzaSyBjkO2qYjqUWPq8GyF1pfN_tfR55laNTC8",
@@ -56,4 +8,49 @@ var config = {
   messagingSenderId: "344444587997"
 };
 firebase.initializeApp(config);
->>>>>>> master
+
+var params = {
+  // Request parameters
+};
+
+$.ajax({
+  url: "https://api.fantasydata.net/v3/nba/scores/json/TeamSeasonStats/2019" + $.param(params),
+  beforeSend: function (xhrObj) {
+    // Request headers
+    xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "ea390d2b909e42919428428303b716b8");
+  },
+  type: "GET",
+  // Request body
+  data: "{body}",
+})
+  .done(function (data) {
+
+
+    for (var i = 0; i < data.length; i++)
+      $('<option/>').val(data[i].Name).html(data[i].Name).appendTo('#TeamControlSelect');
+
+    console.log(data[0].Name);
+    console.log(data[0].Wins);
+
+
+    $("#submit").on("click", function (event) {
+      event.preventDefault()
+      console.log("ive been clicked")
+
+
+
+
+
+
+    })
+
+
+
+
+
+
+  })
+  .fail(function () {
+    alert("error");
+  });
+
